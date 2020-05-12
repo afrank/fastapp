cd9a6f48dbd0e104e04b2b97464fc1b70522a180
Adam Frank <afrank@wit.com>
2020-05-11 18:25:47 -0700

    more stuff

--- fastapp/__init__.py
+++ fastapp/__init__.py
@@ -1,12 +1,27 @@
 
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 import logging
+import argparse
+import json
 
 class Router(BaseHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        for route in routes:
+            if self.command not in route.get("methods"):
+                continue
+            if self.path == route.get("path"):
+                pkg = importlib.import_module(route.get("pkg"))
+                mod = getattr(pkg,route.get("class"))
+                txt = str(mod())
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(bytes(txt, "utf-8"))
+                self.wfile.write(bytes('\n', "utf-8"))
+                return
 
 class App:
     def __init__(self,**kwargs):
+        logging.basicConfig(format="[%(asctime)s] %(name)s [%(levelname)s]: %(message)s", level=logging.INFO)
         self.bind_host = kwargs.get("host", "127.0.0.1")
         self.bind_port = kwargs.get("port", "6000")
         self.router = kwargs.get("router", Router)
@@ -25,4 +40,11 @@ class App:
             pass
         logging.info(f"Stopping server")
         self.server.server_close()
+
+    def cli(self):
+        self.parser = argparse.ArgumentParser(description="cutehttp application server")
+        self.parser.add_argument("-b", "--bind", help="Host address to bind to; default 127.0.0.1", default="127.0.0.1")
+        self.parser.add_argument("-p", "--port", help="Port to bind to; default 6000", default="6000")
+
+        self.args = parser.parse_args()
 
